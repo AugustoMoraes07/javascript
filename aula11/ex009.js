71a11a33f832125fa1cd488cbbb6fe3bcc3f5671
Augusto <augustofelipemf45@gmail.com>
2024-10-13 22:35:28 -0300

    if  e else aula11

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+// BRASILEIRO OU ESTRANGEIRO
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log(`Você é BRASILEIRO!`)
+}else{
+    console.log(`Você é ESTRANGEIRO`)
+}
