4e69b43c1523d81f795b4f60587fd1d19f1686ce
Augusto <augustofelipemf45@gmail.com>
2024-10-15 11:28:24 -0300

    condition multiple

---
+++ aula12/ex013-bonus.js
@@ -0,0 +1,16 @@
+var expressão = 1
+console.log(`Case ${expressão} Aberta`)
+switch (expressão){
+    case 1:
+        console.log('Você ganhou R$1')
+        break
+    case 2:
+        console.log('Você ganhou R$5')
+        break
+    case 3:
+        console.log('Você ganhou R$10')
+        break
+    default:
+        console.log('Parábens VOCÊ não ganhou nada!')
+        break
+}
