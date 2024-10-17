e03b175e13b4bbd09b5a083db5cfcb2da0a59149
Augusto <augustofelipemf45@gmail.com>
2024-10-17 11:01:51 -0300

    update horário.html

--- aula12ex/scripts/hora.js
+++ aula12ex/scripts/hora.js
@@ -1,3 +1,4 @@
+function carregar(){
 var dia = new Date()
 var hora  = dia.getHours()
 var min = dia.getMinutes()
@@ -18,15 +19,15 @@ if(hora < 10 && min < 10){
 // Background BODY and IMGH
 if (hora < 12){
     //MANHA
-    imgh.style.backgroundImage = 'url(image/manha.jpg)'
-    document.body.style.backgroundColor = 'lightsalmon'
-
+    imgh.style.backgroundImage = 'url(image/manhaoriginal.jpg)'
+    document.body.style.backgroundColor = '#af8f61'
 } else if (hora <=18){
    // TARDE
-    imgh.style.backgroundImage = 'url(image/tarde.jpg)'
-    document.body.style.backgroundColor = '#ee6436'
+    imgh.style.backgroundImage = 'url(image/tardeoriginal.jpg)'
+    document.body.style.backgroundColor = '#82281c'
 } else if (hora > 18){
    // NOITE
-    imgh.style.backgroundImage = 'url(image/noite.jpg)'
-    document.body.style.backgroundColor = '#036582'
+    imgh.style.backgroundImage = 'url(image/noiteoriginal.jpg)'
+    document.body.style.backgroundColor = '#3e4855'
+}
 }
