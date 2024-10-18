3606a058ac4882cb5b56bd9f627f9083d7fb3b8f
Augusto <augustofelipemf45@gmail.com>
2024-10-18 12:12:44 -0300

    ex015 verificador de idade

--- aula12ex/ex015/scripts/verificadoridade.js
+++ aula12ex/ex015/scripts/verificadoridade.js
@@ -1,8 +1,57 @@
-function verificador(){
+var data = new Date()
+var ano = data.getFullYear()
+
+function verificador(){ //FUNÇÃO PARA VERIFICAR A IDADE
     var nasc = document.getElementById('nasc')
     var sx = document.getElementsByName('sx')
     var txti = document.querySelector('div#txtimagem') 
-    var i = document.querySelector('div#imagem')
-    var idade = 2024 - Number(nasc.value)
-    txti.innerHTML = `Você tem ${idade} anos !`
+    var idade = ano - Number(nasc.value)
+    var img = document.createElement('img')
+    img.setAttribute('id','foto')
+
+    //MSG [ERRO] CASO O ANO INSERIDO SEJA INVÁLIDO
+    
+    if(Number(nasc.value) < 0 || Number(nasc.value) > ano){
+        window.alert('[ERRO] Dados Inválidos, tente novamente!')
+    }else{
+
+        
+        //VERIFICAR O GÊNERO DA PESSOA
+        var gênero = ''
+        if(sx[0].checked){
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 13){
+                //BEBE M
+                img.setAttribute('src','bebe-m.jpeg')
+            }else if(idade < 22){
+                //JOVEM M
+                img.setAttribute('src','jovem-m.jpeg')
+            } else if(idade < 60){
+                //ADULTO M
+                img.setAttribute('src','adulto-m.jpeg')
+            } else{
+                //IDOSO M
+                img.setAttribute('src','idoso-m.jpeg')
+            }
+        }else if(sx[1].checked){
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 13){
+                //BEBE F
+                img.setAttribute('src','bebe-f.jpeg')
+            }else if(idade < 22){
+                //JOVEM F
+                img.setAttribute('src','jovem-f.jpeg')
+            } else if(idade < 60){
+                //ADULTO F
+                img.setAttribute('src','adulto-f.jpeg')
+            } else{
+                //IDOSO F
+                img.setAttribute('src','idoso-f.jpeg')
+            }
+            }
+    
+    // QUANTOS ANOS A PESSOA TEM E SEU GÊNERO
+    txti.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    txti.appendChild(img)
+   }
 }
