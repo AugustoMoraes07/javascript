c303ab9d1c46305cd803e5be5fadc1a87bda3c22
Augusto <augustofelipemf45@gmail.com>
2024-10-22 09:48:56 -0300

    estrutura de repetição for

---
+++ aula14 (for)/contandoalunos.js
@@ -0,0 +1,16 @@
+var aluno = 1
+do{
+    console.log(`Aluno ${aluno}`)
+    aluno++
+}while(aluno <= 10)
+
+
+/*CONTANDO 10 ALUNOS COM WHILE NO COMEÇO
+    while(aluno <= 10){
+        console.log(`Aluno ${aluno}`)
+        aluno++
+    }
+*/
+
+//AMBAS TEM A MESMA FUNCIONALIDADE, PORÉM EM CERTAS OCASIÕES UMA É MELHOR QUE A OUTRA
+    
---
+++ aula14 (for)/ex17/for.js
@@ -0,0 +1,13 @@
+/* var c = 1
+while (c <= 10){
+    console.log(c+'..')
+    c++
+}
+console.log('FIM') */
+
+
+/* NESSE CASO E PARA OUTROS DIVERSOS CASOS A ESTRUTURA DE REPETIÇÃO (FOR) TEM A MESMA FUNCIONALIDADE DAS OUTRAS ESTRUTURAS (WHILE E DO_WHILE)*/
+for(var c = 1; c <= 10; c++){ 
+    console.log(c+'..')
+}
+console.log('FIM')
