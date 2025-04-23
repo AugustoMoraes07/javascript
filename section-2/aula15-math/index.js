3802e12cfc681e83b0d056c20eaaaaf323a093e5
Augusto <augustofelipemf45@gmail.com>
2025-04-23 13:38:27 -0300

    object math

---
+++ section-2/aula15-math/index.js
@@ -0,0 +1,8 @@
+let num1 = 9.454;
+console.log(num1 ** 0.5); // pegar raiz quadrada
+//let num2 = Math.floor(num1); arredondar para baixo
+//let num2 = Math.ceil(num1); arredondar para cima
+// let num2 = Math.round(num1) arredondar para o mais próximo
+//console.log(Math.max(num1,num2)) maior valor
+//console.log(Math.min(1,2,3,4,5,6,10)) menor valor
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
