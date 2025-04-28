eb746fa216af895f1c58955a54a5947842bae788
Augusto <augustofelipemf45@gmail.com>
2025-04-28 12:56:17 -0300

    16-arrays

---
+++ section-2/aula16-arrays/index.js
@@ -0,0 +1,32 @@
+//const alunos = 'Luiz Maria João';
+//const alunosArray = alunos.split(' '); convertendo para array
+
+//               0          1        2         3          4
+let alunos = ['Augusto', 'Maria', 'João']; // 'Matheus', 'Nicollas'
+// Alunos é uma instancia de Array?
+console.log
+console.log(alunos instanceof Array)
+
+// fatiando valores
+// alunos.push('Matheus')
+// alunos.push('Nicollas')
+// console.log(alunos.slice(0,-3))
+
+//console.log(alunos[30]); indice não definido = undefined
+
+//Removendo somente dados do indice
+//delete alunos[0]
+//console.log(alunos);
+
+// Removendo indice e dados 
+//const removidos = alunos.shift(); remover começo
+//const removidos = alunos.pop(); remover final
+
+//alunos[0] = 'Felipe'; substituindo 
+//
+// ADD
+//alunos[alunos.length] = 'Eduardo'; adicionando
+//alunos.push('Eduardo') adicionar de forma mais prática
+//alunos.unshift('Felipe') adicionar no começo
+//
+
