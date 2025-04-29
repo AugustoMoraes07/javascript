7dc38eb4da0228b1aa9a7f1fcdb90a9a91306261
Augusto <augustofelipemf45@gmail.com>
2025-04-29 16:19:58 -0300

    starting functions

---
+++ section-2/aula17-functions/index.js
@@ -0,0 +1,10 @@
+
+
+function cumprimentar(nome){
+    console.log(`Olá ${nome}, como você está?`)
+    return '424.683.345-67'
+    
+}
+
+const funcao = cumprimentar('Cleriton Savio');
+console.log(`Seu cpf é ${funcao} e foi registrado com sucesso.`);
---
+++ section-2/tests/index.js
@@ -0,0 +1,13 @@
+// Mutável ou imutável em const
+
+const valor = [10]
+valor.push(9)
+valor.unshift(8)
+valor.unshift(7)
+valor.unshift(6)
+valor.unshift(5)
+valor.unshift(4)
+valor.unshift(3)
+valor.unshift(2)
+valor.unshift(1)
+console.log(valor)
