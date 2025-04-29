bfb53d64aa3abc40ab0a9da0f9a667f465b01de5
Augusto <augustofelipemf45@gmail.com>
2025-04-29 18:51:15 -0300

    function basic

--- section-2/aula17-functions/index.js
+++ section-2/aula17-functions/index.js
@@ -1,10 +1,21 @@
-
-
-function cumprimentar(nome){
-    console.log(`Olá ${nome}, como você está?`)
-    return '424.683.345-67'
-    
+// Função que retorna valor e tem uma pré-definição
+/*function soma(x=0,y=0){
+    const resultado = x+y;
+    return resultado
 }
+/
+const resultado = soma();
+console.log(resultado);*/
+//
+// Função que retorna valor
+/*function cumprimentar(nome){
+    return `Olá ${nome}, seja bem vindo(a)!`;
+}
+/
+const nome = cumprimentar(`Felipe`);
+console.log(nome);*/
+//
 
-const funcao = cumprimentar('Cleriton Savio');
-console.log(`Seu cpf é ${funcao} e foi registrado com sucesso.`);
+// Função Anônima e Arrow Function
+const raiz = n => Math.round(n ** 0.5);
+console.log(raiz(9));
