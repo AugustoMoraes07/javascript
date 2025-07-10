d734dcd9e012dbbf9145ce8d312b1eeeb815c760
Augusto <augustofelipemf45@gmail.com>
2025-07-10 17:56:33 -0300

    return default and with object

---
+++ section-4-functions/aula51-retorno/index.js
@@ -0,0 +1,20 @@
+// return
+// Retorna um valor
+// Termina a função
+
+// function soma(a,b){
+//     return a + b;
+// }
+//  
+// soma(2,3);
+
+// function com return e objeto normal
+// function criaPessoa(nome, sobrenome){
+//     return{nome, sobrenome};
+// }
+
+// const p1 = criaPessoa('Augusto', 'Moraes');
+// const p2 = {nome: 'Jao', sobrenome: 'Oliveira'};
+
+// console.log(typeof p1);
+// console.log(typeof p2);
