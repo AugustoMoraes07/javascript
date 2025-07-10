93632f8dfd1f529a86f2dca49acf918804626e93
Augusto <augustofelipemf45@gmail.com>
2025-07-10 18:17:01 -0300

    function creating function

--- section-4-functions/aula51-retorno/index.js
+++ section-4-functions/aula51-retorno/index.js
@@ -12,9 +12,21 @@
 // function criaPessoa(nome, sobrenome){
 //     return{nome, sobrenome};
 // }
-
 // const p1 = criaPessoa('Augusto', 'Moraes');
 // const p2 = {nome: 'Jao', sobrenome: 'Oliveira'};
-
 // console.log(typeof p1);
 // console.log(typeof p2);
+
+
+
+function falaFrase(comeco){
+    function falaResto(resto){
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const fala = falaFrase('Valen');
+const resto = fala('tina');
+
+console.log(resto);
