c3da4d6e6c087b281070b2dcdb6caf9e6469b924
Augusto <augustofelipemf45@gmail.com>
2025-07-11 20:38:48 -0300

    finish introduction for closure and starting practice

--- section-4-functions/aula53-closures/index.js
+++ section-4-functions/aula53-closures/index.js
@@ -1,9 +1,12 @@
 // Global
-function retornafuncao(){
-    const nome = 'Augusto'; 
+function retornafuncao(nome){
     return function(){
         return nome;
     };
 }
-const funcao = retornafuncao();
-console.log(funcao);
+const funcao = retornafuncao('Augusto');
+const funcao2 = retornafuncao('Felipe');
+console.dir(funcao);
+console.dir(funcao2);
+
+console.log(funcao(), funcao2());
