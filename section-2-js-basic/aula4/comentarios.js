948f64b6d189fc70d68bffe55e06f7ff5a3688f3
Augusto <augustofelipemf45@gmail.com>
2026-01-05 16:05:35 -0300

    comment

---
+++ section-2-js-basic/aula4/comentarios.js
@@ -0,0 +1,18 @@
+// Escrevendo um comentário...
+/* 
+    Comentário
+    Longo 
+    ou 
+    Bloco 
+    de 
+    Código
+*/
+console.log('Olá mundo!'); // escrevendo outro comentário...
+console.log('Linha 1');
+/*
+// Comentário no meio... 
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// Vai ser ignorado TCHAU
