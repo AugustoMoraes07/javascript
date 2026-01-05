b9099349f63b18935ed1967e7a8f50b57e235627
Augusto <augustofelipemf45@gmail.com>
2026-01-05 18:16:08 -0300

    variavel-let

---
+++ section-2-js-basic/aula6/index.js
@@ -0,0 +1,14 @@
+let nome; // Declarou variável
+nome = 'Augusto'; // Inicializando a variável
+console.log(nome);
+nome = 'Felipe';
+console.log(nome);
+
+// Não pode criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não pode conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize var, sempre use let
