04eab62d4171c2073a5166c8fe77b784622ec912
Augusto <augustofelipemf45@gmail.com>
2026-01-05 20:04:45 -0300

    primitive data

--- section-2-js-basic/aula9/index.js
+++ section-2-js-basic/aula9/index.js
@@ -1,4 +1,16 @@
-let nome = 'Augusto'
+const nome = 'Augusto'; // string
+const nome1 = "Augusto"; // string
+const nome2 = `Augusto`; //string
+const num1 = 10; // number
+const num2 = 10.5; // number
+let nomeAluno; // undefined = Não aponta para local nenhum na memória
+let sobrenomeAluno = null; // nulo -> não aponta para lugar nenhum na memória, ta desativado
+const aprovado = true; // boolean -> true or false (lógico)
 
-nome = 'Felipe';
-console.log(nome);
+const a = [1, 2];
+const b = a;
+
+console.log(b, a); 
+
+b.push(3);
+console.log(a, b);
