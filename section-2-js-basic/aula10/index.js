d07df91ff92b12652839532aa6a60c34c50f299e
Augusto <augustofelipemf45@gmail.com>
2026-01-06 09:48:33 -0300

    arithmetic-attribution-conversion

---
+++ section-2-js-basic/aula10/index.js
@@ -0,0 +1,25 @@
+/*
+    Ordem Aritmética
+    () 
+    ** 
+    * / %
+    + -
+
+    Incremento = ++
+    Decremento = --
+
+    Operadores de atribuição
+    variavel **= 10
+
+    NaN - Not a Number
+
+    Conversão - STR for Number
+    parseInt(variavel) - Inteiro
+    parseFloat(variavel) - Flutuante
+    Number(variavel) - Converte ambos
+*/
+
+const num1 = 10;
+const num2 = Number('5.32');
+console.log(num1+num2);
+console.log(typeof num2);
