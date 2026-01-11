a66957ae3394872a7339c2efc4cf7f15bc16f28d
Augusto <augustofelipemf45@gmail.com>
2026-01-10 21:04:40 -0300

    ex aula13

--- section-2-js-basic/aula13/index.js
+++ section-2-js-basic/aula13/index.js
@@ -1,7 +1,8 @@
 //               0123456789
-let umaString = "O rato roeu a roupa do rei de roma.";
+let umaString = "Gratidão pela vida, Deus é meu melhor amigo.";
 
 // console.log(umaString.length -3)
-// console.log(umaString.slice(-12,-1));
-// console.log(umaString.substring(umaString.length - 5, umaString.length - 1));
-console.log(umaString.toUpperCase());
+// console.log(umaString.slice(-6,-1));
+// console.log(umaString.substring(umaString.length - 6, umaString.length - 1));
+// console.log(umaString.toUpperCase());
+// console.log(umaString.replace('a', ' '));
