ce7bb7649dd45de29e506b6ad3786a003aa0621c
Augusto <augustofelipemf45@gmail.com>
2026-01-14 20:54:00 -0300

    function basic

---
+++ section-2-js-basic/aula17/index.js
@@ -0,0 +1,20 @@
+// function soma(x = 0, y = 0) // Pré-definicão de valores
+// {
+//     const resultado = x + y;
+//     return resultado; // Encerra função pós return
+// }
+
+
+// const resultado = soma();
+// console.log(resultado);
+
+// Função Anônima
+// const raiz = function(n) {
+//     return n ** 0.5;
+// };
+// Arrow Function
+const raiz = n => n**0.5;
+
+console.log(raiz(9));
+console.log(raiz(20));
+console.log(raiz(30));
