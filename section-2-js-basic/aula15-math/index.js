f6c9977fd27d64c43dcc9dd6b427591fb5ef49c4
Augusto <augustofelipemf45@gmail.com>
2026-01-13 21:51:28 -0300

    little more about math

--- section-2-js-basic/aula15-math/index.js
+++ section-2-js-basic/aula15-math/index.js
@@ -1,8 +1,12 @@
-let num1 = 9.454;
-console.log(num1 ** 0.5); // pegar raiz quadrada
-//let num2 = Math.floor(num1); arredondar para baixo
-//let num2 = Math.ceil(num1); arredondar para cima
-// let num2 = Math.round(num1) arredondar para o mais próximo
-//console.log(Math.max(num1,num2)) maior valor
-//console.log(Math.min(1,2,3,4,5,6,10)) menor valor
-// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// let num2 = Math.floor(num1); // Arredonda para baixo
+// let num2 = Math.ceil(num1); // Arredonda para cima
+// let num2 = Math.round(num1); // Arredonda para o mais próximo
+// console.log(Math.min(2,3,5,6,7,8,523523235,-432423,-432,6890,90)); // Mostra o menor valor
+// console.log(Math.max(2,3,5,6,7,8,523523235,-432423,-432,6890,90)); // Mostra o maior valor
+// const aleatorio = Math.round(Math.random() * (10-5) + 5); // Pegar número aleatório
+// console.log(num1 ** 1/2); // Raiz quadrada
+// console.log(100/0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006); // Quando se aproxima do zero, vira infinity seu valor
+
+
+
+
