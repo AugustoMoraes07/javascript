3b36dc3b8fd267679169543b9d8f1d4f8c5b4490
Augusto <augustofelipemf45@gmail.com>
2026-01-16 18:33:50 -0300

    operador de comparação

---
+++ section-3-logica/aula21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+== igual
+=== estritamente igual *
+!= diferente de igual
+!== estritamente diferente de igual *
+*/
+const num1 = 10; // number
+const num2 = '10'; // string
+const comp = num1!==num2;
+console.log(comp);
