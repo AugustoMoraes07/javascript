b45704b962474e23b3b347bf5f6588dee46e8c5b
Augusto <augustofelipemf45@gmail.com>
2026-01-22 09:08:36 -0300

    conditions 2

---
+++ section-3-logica/aula25/index.js
@@ -0,0 +1,17 @@
+const numero = 9;
+
+// Se (numero >= 10 && numero <= 5) ocorrer, faça isso {código}
+// Se não, faça isso {código}
+// Achou uma condição true, para o fluxo
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8.');
+} else if (1===1) { // true
+    console.log('Literal');
+} else if (numero >= 9 && numero <= 11) { // true
+    console.log('O número está entre 9 e 11');
+} else {
+    console.log('O número não está entre 0 e 11.');
+}
