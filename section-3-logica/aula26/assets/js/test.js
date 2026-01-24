e855183c35ed3e4402577cf0c22238ec984573bd
Augusto <augustofelipemf45@gmail.com>
2026-01-24 15:50:46 -0300

    add new function

--- section-3-logica/aula26/assets/js/test.js
+++ section-3-logica/aula26/assets/js/test.js
@@ -3,53 +3,55 @@ function meuEscopo() {
     const telaResultado = document.getElementById('res');
 
     btnCalc.addEventListener("click", function (e) {
-        e.preventDefault(); // Interrompa o envio dos dados
+        // Interrompa o envio dos dados
+        e.preventDefault(); 
+
         // Pegar os dados
         const peso = document.getElementById('peso');
         const altura = document.getElementById('altura');
 
         // Verificação se ambos valores são números e as ações
-        if(numeroOuNao(peso.value) === true && numeroOuNao(altura.value) === true){
-            telaResultado.innerHTML = `Seu peso e altura estão válidos\n`+
-                                      `O seu imc é ${calcImc(peso.value, altura.value)}`;
-        
-        } else{
+        if (numeroOuNao(peso.value) === true && numeroOuNao(altura.value) === true) {
+            const imc = calcImc(peso.value, altura.value);
+            telaResultado.innerHTML = `<p>O seu IMC é ${imc}</p>`;
+            telaResultado.innerHTML += `<p>Nível: ${statsImc(Number(imc))}</p>`;
+        } else {
             telaResultado.innerHTML = `[ERRO] Digite um valor`;
         }
-        
-        
-        
     })
-    
-    
+
+
     // Função para validar se realmente é um número
     function numeroOuNao(num = 0) {
         // Após tudo, alterar para detectar , e substituir pelo . function subsVirgula() {}
         const convertendoNum = Number(num);
-        const EhNumero = !Number.isNaN(convertendoNum); 
+        const EhNumero = !Number.isNaN(convertendoNum);
         return EhNumero;
-}
+    }
 
     // Calcular o imc
-    function calcImc(peso, altura){
-        const imc = peso/(altura*altura);
+    function calcImc(peso, altura) {
+        const imc = peso / (altura * altura);
         return imc.toFixed(2);
+    }
+
+    // Nível/Grau de imc de acordo com seu imc (Condicionais)
+    function statsImc(imc) {
+        if (imc < 18.5) {
+            return 'Abaixo do peso';
+        } else if (imc < 24.9) {
+            return 'Peso normal'
+        } else if (imc < 29.9) {
+            return 'Sobrepeso'
+        } else if(imc < 34.9){
+            return 'Obesidade grau 1'
+        } else if(imc < 39.9){
+            return 'Obesidade grau 2'
+        } else{
+            return 'Obesidade grau 3'
+        }
     }
 }
 
-meuEscopo();
 
-
- // Validação de número (Não está funcionando corretamente)
-        // if (numeroOuNao(peso) === true && numeroOuNao(altura) === true) {
-        //     // Fazer o cálculo de imc
-        //     peso = Number(peso);
-        //     altura = Number(altura);
-        //     const seuImc = (peso) / (altura * altura);
-        //     alert();
-            
-        //     // Mandar para tela de resultado o imc
-        //     telaResultado.innerHTML = `<p>Seu imc é ${seuImc.value}</p>`;
-        // } else {
-        //     // Dar um aviso de erro
-        // }
+    meuEscopo();
