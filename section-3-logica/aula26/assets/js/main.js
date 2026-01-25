e4ceae0884c74b54636e65f96dc9868cd44058a8
Augusto <augustofelipemf45@gmail.com>
2026-01-24 21:33:54 -0300

    finish the exercice01

--- section-3-logica/aula26/assets/js/main.js
+++ section-3-logica/aula26/assets/js/main.js
@@ -2,37 +2,96 @@ function meuEscopo() {
     const btnCalc = document.getElementById('calculo');
     const telaResultado = document.getElementById('res');
 
-    btnCalc.addEventListener("submit", function (e) {
-        e.preventDefault(); // Interrompa o envio dos dados
+    btnCalc.addEventListener("click", function (e) {
+        // Interrompa o envio dos dados
+        e.preventDefault();
 
         // Pegar os dados
-        let peso = document.getElementById('peso');
-        let altura = document.getElementById('altura');
+        const peso = document.getElementById('peso');
+        const altura = document.getElementById('altura');
 
+        // Verificar valores são números e executa as funções
+        if (numeroOuNao(peso.value) === true && numeroOuNao(altura.value) === true) {
 
-        // Após tudo, alterar para detectar , e substituir pelo . function subsVirgula() {}
+            // Verificar se os valores são números válidos
+            if (realMetrics(peso.value, altura.value) === true) {
+                // Passou na validação
+                greenMsg(peso.value, altura.value);
 
-        // Validação de número
-        if (numeroOuNao(peso) === true && numeroOuNao(altura) === true) {
-            // // Fazer o cálculo de imc
-            // peso = Number(peso);
-            // altura = Number(altura);
-            // const seuImc = (peso) / (altura * altura);
-
-            // Mandar para tela de resultado o imc
-            // telaResultado.innerHTML = `<p>Seu imc é ${seuImc.value}</p>`;
-            alert(`O seu imc é ${seuImc}`);     
+            } else {
+                // Falhou na validação
+                redMsg();
+            }
         } else {
-            alert();     
-            telaResultado.innerHTML = `<p>Digite algo válido</p>`;
+            // Falhou na validação
+            redMsg();
         }
     })
 
-    // Validar se realmente é um número
-    function numeroOuNao(numero) {
-        const convert = Number(numero);
-        return !Number.isNaN(convert);
+
+    // Função para validar se realmente é um número
+    function numeroOuNao(num) {
+        // Após tudo, alterar para detectar , e substituir pelo . function subsVirgula() {}
+        const convertendoNum = Number(num);
+        const EhNumero = !Number.isNaN(convertendoNum);
+        return EhNumero;
     }
+
+    // Calcular o imc
+    function calcImc(p, a) {
+        const imc = p / (a * a);
+        return imc.toFixed(2);
+    }
+
+    // Nível/Grau de imc de acordo com seu imc (Condicionais)
+    function statsImc(i) {
+        if (i < 18.5) return 'Abaixo do peso';
+        if (i < 24.9) return 'Peso normal';
+        if (i < 29.9) return 'Sobrepeso';
+        if (i < 34.9) return 'Obesidade grau 1';
+        if (i < 39.9) return 'Obesidade grau 2';
+        if (i > 39.9) return 'Obesidade grau 3';
+    }
+
+    // Caso número seja válido
+    function greenMsg(p, a) {
+        telaResultado.classList.remove('red-msg');
+        telaResultado.classList.add('green-msg');
+
+        const imc = calcImc(p, a);
+        telaResultado.innerHTML = `<p>O seu IMC é ${imc}</p>`;
+        telaResultado.innerHTML += `<p>Nível: ${statsImc(Number(imc))}</p>`;
+    }
+
+    // Caso número seja inválido
+    function redMsg() {
+        telaResultado.classList.remove('green-msg');
+        telaResultado.classList.add('red-msg');
+
+        telaResultado.innerHTML = `<p>[ERRO] Digite um valor válido</p>`;
+    }
+
+
+    function realMetrics(p, a) {
+        const maiorAltura = 3;
+        const menorAltura = 0.546;
+        const maiorPeso = 635;
+        const menorPeso = 2;
+
+        if (a > maiorAltura) {
+            return false;
+        } else if (a < menorAltura) {
+            return false;
+        } else if (p > maiorPeso) {
+            return false;
+        } else if (p < menorPeso) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
+
 
 meuEscopo();
--- section-3-logica/aula26/assets/js/test.js
+++ section-3-logica/aula26/assets/js/test.js
@@ -1,57 +1,24 @@
-function meuEscopo() {
-    const btnCalc = document.getElementById('calculo');
-    const telaResultado = document.getElementById('res');
+function verdadeiraMetrica(a,p){
 
-    btnCalc.addEventListener("click", function (e) {
-        // Interrompa o envio dos dados
-        e.preventDefault(); 
-
-        // Pegar os dados
-        const peso = document.getElementById('peso');
-        const altura = document.getElementById('altura');
-
-        // Verificação se ambos valores são números e as ações
-        if (numeroOuNao(peso.value) === true && numeroOuNao(altura.value) === true) {
-            const imc = calcImc(peso.value, altura.value);
-            telaResultado.innerHTML = `<p>O seu IMC é ${imc}</p>`;
-            telaResultado.innerHTML += `<p>Nível: ${statsImc(Number(imc))}</p>`;
-        } else {
-            telaResultado.innerHTML = `[ERRO] Digite um valor`;
-        }
-    })
-
-
-    // Função para validar se realmente é um número
-    function numeroOuNao(num = 0) {
-        // Após tudo, alterar para detectar , e substituir pelo . function subsVirgula() {}
-        const convertendoNum = Number(num);
-        const EhNumero = !Number.isNaN(convertendoNum);
-        return EhNumero;
-    }
-
-    // Calcular o imc
-    function calcImc(peso, altura) {
-        const imc = peso / (altura * altura);
-        return imc.toFixed(2);
-    }
-
-    // Nível/Grau de imc de acordo com seu imc (Condicionais)
-    function statsImc(imc) {
-        if (imc < 18.5) {
-            return 'Abaixo do peso';
-        } else if (imc < 24.9) {
-            return 'Peso normal'
-        } else if (imc < 29.9) {
-            return 'Sobrepeso'
-        } else if(imc < 34.9){
-            return 'Obesidade grau 1'
-        } else if(imc < 39.9){
-            return 'Obesidade grau 2'
-        } else{
-            return 'Obesidade grau 3'
-        }
+    const maiorAltura = 3;
+    const menorAltura = 0.546;
+    const maiorPeso = 635;
+    const menorPeso = 2;
+    
+    
+    
+    
+    if (a > maiorAltura) {
+        return false;
+    } else if (a < menorAltura) {
+        return false;
+    } else if (p > maiorPeso) {
+        return false;
+    } else if (p < menorPeso) {
+        return false;
+    } else {
+        return true;
     }
 }
 
-
-    meuEscopo();
+console.log(verdadeiraMetrica(0, 0));
