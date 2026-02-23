dee02be8306a713dee85d62dc596db8606c7b8b5
Augusto <augustofelipemf45@gmail.com>
2026-02-23 06:58:03 -0300

    var-let scope

---
+++ section-3-logica/aula31/index.js
@@ -0,0 +1,28 @@
+let nome;
+let sobrenome = 'Moraes';
+
+// const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco}
+
+// let nome = 'Augusto'; // criando
+// var nome2 = 'Felipe'; // criando
+
+
+// if(verdadeira){
+//     let nome = 'João'; // criando
+//     var nome2 = 'Lucas'; // criando
+    
+//     if(verdadeira){
+//         let nome = 'Outra coisa'; // Criando
+//         var nome2 = 'Gustavo'; // Redeclarada
+//     }
+// }
+
+// function falaOi(){
+//     var nome = 'Augusto';
+// }
+
+
+nome = 'Augusto';
+console.log(nome);
